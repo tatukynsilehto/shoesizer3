d7a52150f7ec76e6622c10c534028ab9a1a65ecb
Tatu Kynsilehto <tatu.kynsilehto@hotmail.com>
2021-07-04 19:00:10 +0300

    puma & toiminnallisuus

--- app/src/main/java/com/example/shoesizer3/MainActivity.java
+++ app/src/main/java/com/example/shoesizer3/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.shoesizer3;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,6 +16,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button vaihdanakyma;
+    Button measureAnk;
+    Button info;
     int selectedBrand;
 
     @Override
@@ -23,6 +28,33 @@ public class MainActivity extends AppCompatActivity {
         String[] brands = new String[]{
                 "Nike", "Adidas", "Puma"
         };
+
+        measureAnk = findViewById(R.id.measurebutton);
+        measureAnk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.infinityshoes.com/size-chart"));
+                startActivity(browserIntent);
+            }
+        });
+
+        info = findViewById(R.id.button6);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setCancelable(true);
+                builder.setMessage("All of the size conversions on this app are estimates. For accurate sizing of a shoe you should always try it on.");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                }); builder.show();
+            }
+        });
 
         Spinner s = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, brands);
